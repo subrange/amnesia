5bb0df9be3420976d750595de3e1bd90fd11c694
Ramon Asuncion <asuncionbatista@gmail.com>
2025-10-14 02:34:11 -0400

    Add stack A, B, and C

--- amnesia.hs
+++ amnesia.hs
@@ -1,7 +1,25 @@
 import System.Environment (getArgs)
 
+data S = A | B | C
+  deriving(Show, Eq)
+
+data Stacks = Stacks
+  { a   :: [Int]
+  , b   :: [Int]
+  , c   :: [Int]
+  , sel :: S
+  }
+
+empty :: Stacks
+empty = Stacks [] [] [] A
+
+instance Show Stacks where
+  show (Stacks a b c sel) =
+    "A: " ++ show a ++ " B: " ++ show b ++ " C: " ++ show c ++ " sel: " ++ show sel
+
 main :: IO ()
 main = do
+  print empty
   args <- getArgs
   case args of
     (filename:_) -> do
