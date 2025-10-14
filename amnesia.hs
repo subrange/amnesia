66bec41f402f2f0d240697a7d38a20c087110776
Ramon Asuncion <asuncionbatista@gmail.com>
2025-10-14 02:01:13 -0400

    Read program from args

--- amnesia.hs
+++ amnesia.hs
@@ -1,1 +1,11 @@
-main = putStrLn "Hello, world!"
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    (filename:_) -> do
+        contents <- readFile filename
+        putStrLn contents
+    [] -> putStrLn "Usage: runghc amnesia.hs <program.amn>"
+
